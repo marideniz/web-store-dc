00c3ae579d0e8533abaabfb872e943ffd4a63a8c
marideniz <mari28deniz@gmail.com>
2024-10-13 03:52:38 +0200

    Update layout.tsx

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 import './AboutStyles.css'
 import AboutNavigation from "@/components/aboutNavigation";
 
-export const metadata: Metadata = {
-    title: 'About | MariDenizDesign',
-    description: 'Shop unique fashion clothing with signature designs',
-    keywords: ['Clothing', 'fashion', 'women\'s clothing', 'men\'s clothing'],
-    openGraph: {
-        images: 'https://res.cloudinary.com/dzdmstsam/image/upload/v1718646315/Letter_-_16_zwgkz9.png',
-    },
-    icons: 'https://res.cloudinary.com/dzdmstsam/image/upload/v1718646289/Letter_-_126_x8fpma.png',
-}
+// export const metadata: Metadata = {
+//     title: 'About | MariDenizDesign',
+//     description: 'Shop unique fashion clothing with signature designs',
+//     keywords: ['Clothing', 'fashion', 'women\'s clothing', 'men\'s clothing'],
+//     openGraph: {
+//         images: 'https://res.cloudinary.com/dzdmstsam/image/upload/v1718646315/Letter_-_16_zwgkz9.png',
+//     },
+//     icons: 'https://res.cloudinary.com/dzdmstsam/image/upload/v1718646289/Letter_-_126_x8fpma.png',
+// }
 
 export default function AboutLayout({children}: { children: React.ReactNode }) {
     return (
