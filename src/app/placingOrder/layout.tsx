0a23792ef606f21cca9c1dbc7e9ea50321f139df
marideniz <mari28deniz@gmail.com>
2024-10-13 03:58:01 +0200

    Update layout.tsx

--- src/app/placingOrder/layout.tsx
+++ src/app/placingOrder/layout.tsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 import './placingOrder.css'
 import {usePathname} from "next/navigation";
 
-export const metadata: Metadata = {
-    title: 'Order | MariDenizDesign',
-    description: 'Shop unique fashion clothing with signature designs',
-    keywords: ['Clothing', 'fashion', 'women\'s clothing', 'men\'s clothing'],
-    openGraph: {
-        images: 'https://res.cloudinary.com/dzdmstsam/image/upload/v1718646315/Letter_-_16_zwgkz9.png',
-    },
-    icons: 'https://res.cloudinary.com/dzdmstsam/image/upload/v1718646289/Letter_-_126_x8fpma.png',
-}
+// export const metadata: Metadata = {
+//     title: 'Order | MariDenizDesign',
+//     description: 'Shop unique fashion clothing with signature designs',
+//     keywords: ['Clothing', 'fashion', 'women\'s clothing', 'men\'s clothing'],
+//     openGraph: {
+//         images: 'https://res.cloudinary.com/dzdmstsam/image/upload/v1718646315/Letter_-_16_zwgkz9.png',
+//     },
+//     icons: 'https://res.cloudinary.com/dzdmstsam/image/upload/v1718646289/Letter_-_126_x8fpma.png',
+// }
 
 export default function OrderLayout({children}: { children: React.ReactNode }) {
     return (
